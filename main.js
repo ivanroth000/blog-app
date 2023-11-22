989d4a5c1244cf5f7ec0f83be40242bdf0dc0e83
ivan roth <ivanroth000@gmail.com>
2023-11-22 20:35:50 -0300

    mejor dinamismo y comenzamos con el js

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ function restaurarColorLabel(id) {
    label.style.color = 'black'
    label.style.fontSize = '15px'
 }
+
+let formulario = document.querySelector('.form')
+let nombre = document.getElementById('nombre')
+let apellido = document.getElementById('apellido')
+let email = document.getElementById('email')
+let pais = document.getElementById('pais')
+let ciudad = document.getElementById('ciudad')
+let hombre = document.getElementById('hombre')
+let mujer = document.getElementById('mujer')
+let nacimiento = document.getElementById('nacimiento')
+let contrasenia = document.getElementById('contraseña')
+let contraseniaRepetida = document.getElementById('contraseñaRepetida')
+
+formulario.onsubmit = (e) => {
+    e.preventDefault()
+
+   
+
+}
