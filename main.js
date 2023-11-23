eddddc20e8707915f029f8d78b9d07349c299598
ivan roth <ivanroth000@gmail.com>
2023-11-22 22:16:20 -0300

    validé el formulario para que no se envié si hay inputs incompletos

--- main.js
+++ main.js
@@ -19,12 +19,34 @@ let ciudad = document.getElementById('ciudad')
 let hombre = document.getElementById('hombre')
 let mujer = document.getElementById('mujer')
 let nacimiento = document.getElementById('nacimiento')
-let contrasenia = document.getElementById('contraseña')
-let contraseniaRepetida = document.getElementById('contraseñaRepetida')
+let contrasenia = document.getElementById('contrasenia')
+let contraseniaRepetida = document.getElementById('contraseniaRepetida')
+let elementos = document.querySelectorAll('.form input');
+let mensajesDeError = document.querySelectorAll('.form .divError');
+
 
 formulario.onsubmit = (e) => {
     e.preventDefault()
+    let esValido = true
+// Remover la clase 'error' de todos los elementos
+elementos.forEach(elemento => elemento.classList.remove('error'));
 
-   
+// Limpiar el contenido de los mensajes de error
+mensajesDeError.forEach(mensajeDeError => mensajeDeError.innerHTML = '');
+
+elementos.forEach(elemento => {
+    let valorInputSinEspacios = elemento.value.trim();
+    if (valorInputSinEspacios.length === 0) {
+        esValido = false;
+        // Obtener el mensaje de error asociado al elemento actual
+        let mensajeDeError = document.getElementById(`msjError${elemento.id.charAt(0).
+        toUpperCase()}${elemento.id.slice(1)}`);
+        let parrafoError = document.createElement('p')
+        parrafoError.classList.add('msjError')
+        parrafoError.innerHTML = `<p>El campo no puede estar vacío</p>`;
+        mensajeDeError.appendChild(parrafoError)
+    }
+});
+
 
 }
