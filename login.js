d6c5d1765fea8ecb326203859dda0acd0504c160
ivan roth <ivanroth000@gmail.com>
2023-11-24 17:31:13 -0300

    mejoré la seguridad y la  validación del formulario

--- login.js
+++ login.js
@@ -25,3 +25,75 @@ export function restaurarColorLabel(ids) {
         }
     });
 }
+
+
+let formulario = document.querySelector('.form')
+let elementos = document.querySelectorAll('.form input');
+let mensajesDeError = document.querySelectorAll('.form .divError');
+let nacimiento = document.getElementById('nacimiento')
+let hombreCheckbox = document.getElementById('hombre');
+let mujerCheckbox = document.getElementById('mujer');
+
+export function validarFormulario (){
+    formulario.onsubmit = (e) => {
+        e.preventDefault()
+        let esValido = true
+    
+        // Limpiar el contenido de los mensajes de error
+        mensajesDeError.forEach(mensajeDeError => mensajeDeError.innerHTML = '');
+
+        elementos.forEach(elemento => {
+            let valorInputSinEspacios = elemento.value.trim();
+            if (valorInputSinEspacios.length === 0) {
+                esValido = false;
+            // Obtener el mensaje de error asociado al elemento actual
+                let mensajeDeError = document.getElementById(`msjError${elemento.id.charAt(0).
+                toUpperCase()}${elemento.id.slice(1)}`);
+                let parrafoError = document.createElement('p')
+                parrafoError.classList.add('msjError')
+                parrafoError.innerText = 'El campo no puede estar vacío';
+                mensajeDeError.appendChild(parrafoError)
+            }
+        });
+        //valida la mayoría de edad del usuario
+        let fechaActual = new Date()
+        const mayoriaDeEdad = 18
+        let edadUsuario = new Date(nacimiento.value)
+        const diferenciaAnios = fechaActual.getFullYear() - edadUsuario.getFullYear();
+        if(diferenciaAnios < mayoriaDeEdad){
+            esValido = false
+            const msjErrorNacimiento = document.querySelector('#msjErrorNacimiento')
+            const msjMenorDeEdad = document.createElement('p')
+            msjMenorDeEdad.classList.add('msjError')
+            msjMenorDeEdad.innerText = 'Debes ser mayor de 18 años para registrarte'
+            msjErrorNacimiento.appendChild(msjMenorDeEdad)
+        }
+        //valida que solo un check-box este seleccionado
+        if((!hombreCheckbox.checked && !mujerCheckbox.checked) || (hombreCheckbox.checked && mujerCheckbox.checked) ){
+            esValido = false
+            let msjErrorSexo = document.querySelector('#msjErrorSexo')
+            let parrafoErrorSexo = document.createElement('p')
+            parrafoErrorSexo.classList.add('msjError')
+            parrafoErrorSexo.innerText = 'Debes seleccionar una opción'
+            msjErrorSexo.appendChild(parrafoErrorSexo)
+        }
+        //validamos que ambas contraseñas coincidan
+        let contrasenia = document.getElementById('contrasenia')
+        let contraseniaRepetida = document.getElementById('contraseniaRepetida')
+        if(contraseniaRepetida.value){
+        if(contrasenia.value !== contraseniaRepetida.value){
+            esValido = false
+            let msjErrorContrasenia = document.querySelector('#msjErrorContraseniaRepetida')
+            let parrafoErrorContrasenia = document.createElement('p')
+            parrafoErrorContrasenia.classList.add('msjError')
+            parrafoErrorContrasenia.innerText = 'Las contraseñas deben coincidir'
+            msjErrorContrasenia.appendChild(parrafoErrorContrasenia)
+        }
+    }
+        //dirige a la otra página si todo está ok
+        if (esValido) {
+            window.location.href = "home.html";
+        }
+
+    }
+}
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // main.js
-import { cambiarColorLabel, restaurarColorLabel } from './login.js';
+import { cambiarColorLabel, restaurarColorLabel, validarFormulario } from './login.js';
 
-
+//validación del formulario
 const idsParaCambiarColor = ['nombre', 'apellido', 'email', 'pais','ciudad', 'hombre','mujer', 'nacimiento', 'contrasenia','contraseniaRepetida'];
-cambiarColorLabel(idsParaCambiarColor);
-restaurarColorLabel(idsParaCambiarColor);
+cambiarColorLabel();
+restaurarColorLabel();
 
 idsParaCambiarColor.forEach(id => {
     const element = document.getElementById(id);
@@ -14,45 +14,4 @@ idsParaCambiarColor.forEach(id => {
     }
 });
 
-let formulario = document.querySelector('.form')
-let nombre = document.getElementById('nombre')
-let apellido = document.getElementById('apellido')
-let email = document.getElementById('email')
-let pais = document.getElementById('pais')
-let ciudad = document.getElementById('ciudad')
-let hombre = document.getElementById('hombre')
-let mujer = document.getElementById('mujer')
-let nacimiento = document.getElementById('nacimiento')
-let contrasenia = document.getElementById('contrasenia')
-let contraseniaRepetida = document.getElementById('contraseniaRepetida')
-let elementos = document.querySelectorAll('.form input');
-let mensajesDeError = document.querySelectorAll('.form .divError');
-
-
-function validarFormulario (formulario){
-    formulario.onsubmit = (e) => {
-    e.preventDefault()
-    let esValido = true
-
-// Limpiar el contenido de los mensajes de error
-mensajesDeError.forEach(mensajeDeError => mensajeDeError.innerHTML = '');
-
-elementos.forEach(elemento => {
-    let valorInputSinEspacios = elemento.value.trim();
-    if (valorInputSinEspacios.length === 0) {
-        esValido = false;
-        // Obtener el mensaje de error asociado al elemento actual
-        let mensajeDeError = document.getElementById(`msjError${elemento.id.charAt(0).
-        toUpperCase()}${elemento.id.slice(1)}`);
-        let parrafoError = document.createElement('p')
-        parrafoError.classList.add('msjError')
-        parrafoError.innerHTML = `<p>El campo no puede estar vacío</p>`;
-        mensajeDeError.appendChild(parrafoError)
-    }
-});
-
-if (esValido) {
-    window.location.href = "home.html";
-}
-
-}}
+validarFormulario()
